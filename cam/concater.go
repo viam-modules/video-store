042283536b83e67cb8b957e8d521fd1538f97c82
seanavery <seanpollock3344@gmail.com>
2024-09-03 12:10:49 -0400

    Add comments to concat flow

--- cam/concater.go
+++ cam/concater.go
@@ -49,33 +49,24 @@ func newConcater(logger logging.Logger, storagePath string, uploadPath string, c
 // concat takes in from and to timestamps and concates the video files between them.
 // returns the path to the concated video file.
 func (c *concater) concat(from time.Time, to time.Time, metadata string) (string, error) {
-	// get list of files in storagePath
 	storageFiles, err := getSortedFiles(c.storagePath)
 	if err != nil {
 		c.logger.Error("failed to get sorted files", err)
 		return "", err
 	}
 	if len(storageFiles) == 0 {
-		return "", errors.New("no video data to save")
+		return "", errors.New("no video data in storage")
 	}
-	// validate time range against storage files
-	minStorageTime, err := extractDateTimeFromFilename(storageFiles[0])
+	err = validateTimeRange(storageFiles, from, to)
 	if err != nil {
 		return "", err
 	}
-	maxStorageTime, err := extractDateTimeFromFilename(storageFiles[len(storageFiles)-1])
-	if err != nil {
-		return "", err
-	}
-	if from.Before(minStorageTime) || to.After(maxStorageTime) {
-		return "", errors.New("time range is out of bounds")
-	}
-	// get matching storage files that are within the time range
 	matchingFiles := matchStorageToRange(storageFiles, from, to)
 	if len(matchingFiles) == 0 {
-		return "", errors.New("no video data to save")
+		return "", errors.New("no matching video data to save")
 	}
-	// clear the concat file and write the matching files to it
+
+	// Clear the concat file and write the matching files list to it.
 	c.concatFile.Truncate(0)
 	c.concatFile.Seek(0, 0)
 	for _, file := range matchingFiles {
@@ -95,7 +86,9 @@ func (c *concater) concat(from time.Time, to time.Time, metadata string) (string
 		return "", errors.New("failed to find input format")
 	}
 
-	// open the input format context
+	// Open the input format context with the concat demuxer.
+	// This block sets up the input format context to read the concatenated input files.
+	// It uses the concat demuxer with the 'safe' option set to '0' to allow absolute paths in the input file list.
 	var options *C.AVDictionary
 	safeStr := C.CString("safe")
 	safeValStr := C.CString("0")
@@ -108,7 +101,6 @@ func (c *concater) concat(from time.Time, to time.Time, metadata string) (string
 	if ret < 0 {
 		return "", fmt.Errorf("failed to open input format: %s", ffmpegError(ret))
 	}
-	//  get the input stream info
 	ret = C.avformat_find_stream_info(inputCtx, nil)
 	if ret < 0 {
 		return "", fmt.Errorf("failed to find stream info: %s", ffmpegError(ret))
@@ -134,56 +126,53 @@ func (c *concater) concat(from time.Time, to time.Time, metadata string) (string
 		return "", fmt.Errorf("failed to allocate output context: %s", ffmpegError(ret))
 	}
 
-	// copy the stream info from the input to the output
+	// Copy codec info from input to output context. This is necessary to ensure
+	// we do not decode and re-encode the video data.
 	for i := 0; i < int(inputCtx.nb_streams); i++ {
-		// pointer arithmetic to get the stream
 		inStream := *(**C.AVStream)(unsafe.Pointer(uintptr(unsafe.Pointer(inputCtx.streams)) + uintptr(i)*unsafe.Sizeof(inputCtx.streams)))
 		outStream := C.avformat_new_stream(outputCtx, nil)
 		if outStream == nil {
 			return "", fmt.Errorf("failed to allocate stream")
 		}
-
-		// copy codec parameters from input stream to output stream
 		ret := C.avcodec_parameters_copy(outStream.codecpar, inStream.codecpar)
 		if ret < 0 {
 			return "", fmt.Errorf("failed to copy codec parameters: %s", ffmpegError(ret))
 		}
-
-		// let ffmpeg handle the codec tag for us
+		// Let ffmpeg handle the codec tag for us.
 		outStream.codecpar.codec_tag = 0
 	}
 
-	// open the output file
+	// Open the output file and write the header.
 	ret = C.avio_open(&outputCtx.pb, outputPathCStr, C.AVIO_FLAG_WRITE)
 	if ret < 0 {
 		return "", fmt.Errorf("failed to open output file: %s", ffmpegError(ret))
 	}
-
 	ret = C.avformat_write_header(outputCtx, nil)
 	if ret < 0 {
 		return "", fmt.Errorf("failed to write header: %s", ffmpegError(ret))
 	}
 
+	// Iterate through each packet in the input context and write it to the output context.
+	// TODO(seanp): We can hopefully optimize this by copying input segments entirely instead of packet by packet.
 	packet := C.av_packet_alloc()
 	for {
 		ret := C.av_read_frame(inputCtx, packet)
 		if ret == C.AVERROR_EOF {
-			fmt.Println("EOF")
+			c.logger.Debug("Concatenation complete. Hit EOF.")
 			break
 		}
+		// Any error other than EOF is a problem.
 		if ret < 0 {
 			return "", fmt.Errorf("failed to read frame: %s", ffmpegError(ret))
 		}
-
-		// Adjust the PTS, DTS, and duration correctly for each packet
+		// Adjust the PTS, DTS, and duration correctly for each packet.
+		// Can have multiple streams, so need to adjust each packet based on the stream it belongs to.
 		inStream := *(**C.AVStream)(unsafe.Pointer(uintptr(unsafe.Pointer(inputCtx.streams)) + uintptr(packet.stream_index)*unsafe.Sizeof(uintptr(0))))
 		outStream := *(**C.AVStream)(unsafe.Pointer(uintptr(unsafe.Pointer(outputCtx.streams)) + uintptr(packet.stream_index)*unsafe.Sizeof(uintptr(0))))
 		packet.pts = C.av_rescale_q_rnd(packet.pts, inStream.time_base, outStream.time_base, C.AV_ROUND_NEAR_INF|C.AV_ROUND_PASS_MINMAX)
 		packet.dts = C.av_rescale_q_rnd(packet.dts, inStream.time_base, outStream.time_base, C.AV_ROUND_NEAR_INF|C.AV_ROUND_PASS_MINMAX)
 		packet.duration = C.av_rescale_q(packet.duration, inStream.time_base, outStream.time_base)
 		packet.pos = -1
-
-		// write the packet
 		ret = C.av_interleaved_write_frame(outputCtx, packet)
 		if ret < 0 {
 			return "", fmt.Errorf("failed to write frame: %s", ffmpegError(ret))
