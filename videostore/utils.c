d06ecc844b03381d441c8771b2d36e4dea658c6a
seanavery <seanpollock3344@gmail.com>
2025-02-26 14:42:17 -0500

    Err define

--- videostore/utils.c
+++ videostore/utils.c
@@ -3,11 +3,11 @@
 int64_t get_video_duration(const char *filename) {
     AVFormatContext *fmt_ctx = NULL;
     if (avformat_open_input(&fmt_ctx, filename, NULL, NULL) < 0) {
-        return -1;
+        return VIDEO_STORE_DURATION_RESP_ERROR;
     }
     if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
         avformat_close_input(&fmt_ctx);
-        return -1;
+        return VIDEO_STORE_DURATION_RESP_ERROR;
     }
     int64_t duration = fmt_ctx->duration;
     avformat_close_input(&fmt_ctx);
