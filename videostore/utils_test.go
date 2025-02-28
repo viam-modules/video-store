ee773f607bd4451d3ff6dd4689533cfe4f574ad4
seanavery <seanpollock3344@gmail.com>
2025-02-28 10:52:48 -0500

    Add missing file test case

--- videostore/utils_test.go
+++ videostore/utils_test.go
@@ -9,23 +9,28 @@ import (
 const artifactStoragePath = "../.artifact/data/"
 
 func TestGetVideoDuration(t *testing.T) {
-	t.Run("Valid video file duration", func(t *testing.T) {
+	t.Run("Valid video file duration succeeds", func(t *testing.T) {
 		duration, err := getVideoDuration(artifactStoragePath + "2024-09-06_15-00-03.mp4")
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, duration.Seconds(), test.ShouldBeGreaterThan, 0.0)
 	})
-	t.Run("No duration video file", func(t *testing.T) {
+	t.Run("No duration video file errors", func(t *testing.T) {
 		duration, err := getVideoDuration(artifactStoragePath + "zero_duration_video.mp4")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
 	})
-	t.Run("Not a video file", func(t *testing.T) {
+	t.Run("Not a video file errors", func(t *testing.T) {
 		duration, err := getVideoDuration(artifactStoragePath + "not_video_file.txt")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
 	})
-	t.Run("Broken video file", func(t *testing.T) {
+	t.Run("Broken video file errors", func(t *testing.T) {
 		duration, err := getVideoDuration(artifactStoragePath + "no_moov.mp4")
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
+	})
+	t.Run("Nonexistent file errors", func(t *testing.T) {
+		duration, err := getVideoDuration(artifactStoragePath + "nonexistent.mp4")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
 	})
