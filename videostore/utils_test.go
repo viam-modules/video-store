23bdb363876afce04f0c0b60522edf715fec2677
seanavery <seanpollock3344@gmail.com>
2025-03-07 13:43:01 -0500

    Update utils test

--- videostore/utils_test.go
+++ videostore/utils_test.go
@@ -10,30 +10,30 @@ const artifactStoragePath = "../.artifact/data/"
 
 func TestGetVideoDuration(t *testing.T) {
 	t.Run("Valid video file duration succeeds", func(t *testing.T) {
-		duration, err := getVideoDuration(artifactStoragePath + "2024-09-06_15-00-03.mp4")
+		duration, _, _, _, err := getVideoInfo(artifactStoragePath + "2024-09-06_15-00-03.mp4")
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, duration.Seconds(), test.ShouldEqual, 30.0)
 	})
 	t.Run("No duration video file errors", func(t *testing.T) {
-		duration, err := getVideoDuration(artifactStoragePath + "zero_duration_video.mp4")
+		duration, _, _, _, err := getVideoInfo(artifactStoragePath + "zero_duration_video.mp4")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, err.Error(), test.ShouldContainSubstring, "failed to get video duration")
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
 	})
 	t.Run("Not a video file errors", func(t *testing.T) {
-		duration, err := getVideoDuration(artifactStoragePath + "not_video_file.txt")
+		duration, _, _, _, err := getVideoInfo(artifactStoragePath + "not_video_file.txt")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, err.Error(), test.ShouldContainSubstring, "Invalid data found when processing input")
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
 	})
 	t.Run("Broken video file errors", func(t *testing.T) {
-		duration, err := getVideoDuration(artifactStoragePath + "no_moov.mp4")
+		duration, _, _, _, err := getVideoInfo(artifactStoragePath + "no_moov.mp4")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, err.Error(), test.ShouldContainSubstring, "Invalid data found when processing input")
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
 	})
 	t.Run("Nonexistent file errors", func(t *testing.T) {
-		duration, err := getVideoDuration(artifactStoragePath + "nonexistent.mp4")
+		duration, _, _, _, err := getVideoInfo(artifactStoragePath + "nonexistent.mp4")
 		test.That(t, err, test.ShouldNotBeNil)
 		test.That(t, err.Error(), test.ShouldContainSubstring, "No such file or directory")
 		test.That(t, duration.Seconds(), test.ShouldEqual, 0.0)
