af9c907d9adb4cdd835311e874ed1b048c5e4350
seanavery <seanpollock3344@gmail.com>
2025-03-07 11:03:14 -0500

    WIP get video info helper

--- videostore/utils.c
+++ videostore/utils.c
@@ -1,24 +1,83 @@
 #include "utils.h"
 
-int get_video_duration(int64_t *duration,   // OUT
-                       const char *filename // IN
-) {
+// int get_video_duration(int64_t *duration,   // OUT
+//                        const char *filename // IN
+// ) {
+//     AVFormatContext *fmt_ctx = NULL;
+//     int ret;
+//     if ((ret = avformat_open_input(&fmt_ctx, filename, NULL, NULL)) < 0) {
+//         return ret;
+//     }
+//     if ((ret = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
+//         avformat_close_input(&fmt_ctx);
+//     return ret;
+//     }
+//     *duration = fmt_ctx->duration;
+//     if (*duration == AV_NOPTS_VALUE) {
+//         avformat_close_input(&fmt_ctx);
+//         return VIDEO_STORE_DURATION_RESP_ERROR;
+//     }
+//     avformat_close_input(&fmt_ctx);
+//     return VIDEO_STORE_DURATION_RESP_OK;
+// }
+
+#define CODEC_NAME_LEN 64
+
+int get_video_info(int64_t *duration,   // OUT
+                   int *width,          // OUT
+                   int *height,         // OUT
+                   char *codec,         // OUT
+                   const char *filename // IN
+)
+{
     AVFormatContext *fmt_ctx = NULL;
     int ret;
+
     if ((ret = avformat_open_input(&fmt_ctx, filename, NULL, NULL)) < 0) {
         return ret;
     }
     if ((ret = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
         avformat_close_input(&fmt_ctx);
-    return ret;
+        return ret;
     }
+
     *duration = fmt_ctx->duration;
     if (*duration == AV_NOPTS_VALUE) {
         avformat_close_input(&fmt_ctx);
-        return VIDEO_STORE_DURATION_RESP_ERROR;
+        return VIDEO_STORE_VIDEO_INFO_RESP_ERROR;
     }
+
+    *width = 0;
+    *height = 0;
+    codec[0] = '\0'; // make sure it's empty
+
+    // Choose first video stream and fetch parameters.
+    for (unsigned i = 0; i < fmt_ctx->nb_streams; i++) {
+        AVStream *st = fmt_ctx->streams[i];
+        if (st->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            *width = st->codecpar->width;
+            *height = st->codecpar->height;
+            const char *codecName = avcodec_get_name(st->codecpar->codec_id);
+            if (codecName) {
+                strncpy(codec, codecName, CODEC_NAME_LEN - 1);
+                codec[CODEC_NAME_LEN - 1] = '\0'; // ensure null-termination
+            }
+            break;
+        }
+    }
+
+    if (*width == 0 || *height == 0) {
+        avformat_close_input(&fmt_ctx);
+        return VIDEO_STORE_VIDEO_INFO_RESP_ERROR;
+    }
+
+    if (codec[0] == '\0') {
+        avformat_close_input(&fmt_ctx);
+        return VIDEO_STORE_VIDEO_INFO_RESP_ERROR;
+    }
+
     avformat_close_input(&fmt_ctx);
-    return VIDEO_STORE_DURATION_RESP_OK;
+    return VIDEO_STORE_VIDEO_INFO_RESP_OK;
 }
 
 void custom_av_log_callback(void *ptr, int level, const char *fmt, va_list vargs) {
--- videostore/utils.h
+++ videostore/utils.h
@@ -4,6 +4,8 @@
 int get_video_duration(int64_t *duration, const char *filename);
 void custom_av_log_callback(void *ptr, int level, const char *fmt, va_list vargs);
 void set_custom_av_log_callback();
-#define VIDEO_STORE_DURATION_RESP_ERROR 1
-#define VIDEO_STORE_DURATION_RESP_OK 0
+int get_video_info(int64_t *duration, int *width, int *height, char *codec, const char *filename);
+#define VIDEO_STORE_VIDEO_INFO_RESP_ERROR 1
+#define VIDEO_STORE_VIDEO_INFO_RESP_OK 0
+#define VIDEO_STORE_CODEC_NAME_LEN 64
 #endif /* VIAM_VIDEOSTORE_UTILS_H */
