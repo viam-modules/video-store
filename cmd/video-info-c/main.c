6bef85fb0909f4de8365e2b37d960702491c4708
seanavery <seanpollock3344@gmail.com>
2025-03-12 12:01:24 -0400

    Update video info test bin

--- cmd/video-info-c/main.c
+++ cmd/video-info-c/main.c
@@ -6,16 +6,11 @@ int main(int argc, char *argv[]) {
     printf("usage: %s <video file>\n", argv[0]);
     return 1;
   }
-  int64_t duration;
-  int width;
-  int height;
-  char codec[VIDEO_STORE_CODEC_NAME_LEN];
-  int ret = get_video_info(&duration, &width, &height, codec, argv[1]);
+  VideoInfo info;
+  int ret = get_video_info(&info, argv[1]);
   if (ret == VIDEO_STORE_VIDEO_INFO_RESP_OK) {
-    printf("duration: %ld\n", duration);
-    printf("width: %d\n", width);
-    printf("height: %d\n", height);
-    printf("codec: %s\n", codec);
+    printf("duration: %ld, width: %d, height: %d, codec: %s\n",
+       info.duration, info.width, info.height, info.codec);
   } else {
     printf("error getting video info\n");
   }
