713d8e8a66500ac91f5d30451c31a71312277ede
seanavery <seanpollock3344@gmail.com>
2025-10-09 17:22:56 -0400

    Clean

--- videostore/concat.h
+++ videostore/concat.h
@@ -1,7 +1,6 @@
 #ifndef VIAM_CONCAT_H
 #define VIAM_CONCAT_H
 #include <stdint.h>
-typedef void (*frame_callback_t)(uint8_t *data, int size, int stream_index, int64_t pts, void *user);
 int video_store_concat(const char *concat_filepath, const char *output_path, const char *container);
 #define VIDEO_STORE_CONCAT_RESP_OK 0
 #define VIDEO_STORE_CONCAT_RESP_ERROR 1
