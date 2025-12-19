38d0ef728d34303c24edddb110797ee73154eab6
seanavery <seanpollock3344@gmail.com>
2025-12-19 14:18:35 -0500

    Add fmp4 test

--- videostore/concat_test.go
+++ videostore/concat_test.go
@@ -57,3 +57,86 @@ func TestConcatFaststart(t *testing.T) {
 	test.That(t, mdatOffset, test.ShouldBeGreaterThan, -1)
 	test.That(t, moovOffset, test.ShouldBeLessThan, mdatOffset)
 }
+
+func TestConcatMP4(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+	outputDir := t.TempDir()
+	storagePath := getArtifactStoragePath(t)
+
+	c, err := newConcater(storagePath, outputDir, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	from := time.Unix(1725634803, 0)
+	to := time.Unix(1725634850, 0)
+
+	outputPath := filepath.Join(outputDir, "mp4_test.mp4")
+	err = c.ConcatWithFormat(from, to, outputPath, ContainerMP4)
+	test.That(t, err, test.ShouldBeNil)
+
+	data, err := os.ReadFile(outputPath)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, len(data), test.ShouldBeGreaterThan, 0)
+
+	// Verify ftyp atom exists (MP4 file signature)
+	ftypOffset := findAtomOffset(data, "ftyp")
+	test.That(t, ftypOffset, test.ShouldEqual, 0)
+
+	moovOffset := findAtomOffset(data, "moov")
+	mdatOffset := findAtomOffset(data, "mdat")
+
+	t.Logf("ftyp offset: %d, moov offset: %d, mdat offset: %d", ftypOffset, moovOffset, mdatOffset)
+
+	// With faststart (ContainerMP4), moov should come before mdat
+	// Expected layout: [ftyp][moov][mdat]
+	test.That(t, moovOffset, test.ShouldBeGreaterThan, -1)
+	test.That(t, mdatOffset, test.ShouldBeGreaterThan, -1)
+	test.That(t, moovOffset, test.ShouldBeLessThan, mdatOffset)
+}
+
+func TestConcatFMP4(t *testing.T) {
+	logger := logging.NewTestLogger(t)
+	outputDir := t.TempDir()
+	storagePath := getArtifactStoragePath(t)
+
+	c, err := newConcater(storagePath, outputDir, logger)
+	test.That(t, err, test.ShouldBeNil)
+
+	from := time.Unix(1725634803, 0)
+	to := time.Unix(1725634850, 0)
+
+	outputPath := filepath.Join(outputDir, "fmp4_test.mp4")
+	err = c.ConcatWithFormat(from, to, outputPath, ContainerFMP4)
+	if err != nil {
+		t.Logf("fMP4 concat error: %v", err)
+		if data, readErr := os.ReadFile(outputPath); readErr == nil {
+			t.Logf("Partial file size: %d bytes", len(data))
+		}
+	}
+	test.That(t, err, test.ShouldBeNil)
+
+	data, err := os.ReadFile(outputPath)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, len(data), test.ShouldBeGreaterThan, 0)
+
+	// Verify ftyp atom exists
+	ftypOffset := findAtomOffset(data, "ftyp")
+	test.That(t, ftypOffset, test.ShouldEqual, 0)
+
+	// fMP4 structure without empty_moov:
+	// [ftyp][moov][mdat][moof][mdat][moof][mdat]...
+	// The moov contains initial sample tables, followed by fragmented data
+	moovOffset := findAtomOffset(data, "moov")
+	moofOffset := findAtomOffset(data, "moof")
+
+	t.Logf("ftyp offset: %d, moov offset: %d, moof offset: %d",
+		ftypOffset, moovOffset, moofOffset)
+
+	// moov should exist near the start
+	test.That(t, moovOffset, test.ShouldBeGreaterThan, -1)
+
+	// moof (movie fragment) should exist for fMP4
+	test.That(t, moofOffset, test.ShouldBeGreaterThan, -1)
+
+	// moof should come after moov (fragments come after initial moov)
+	test.That(t, moofOffset, test.ShouldBeGreaterThan, moovOffset)
+}
